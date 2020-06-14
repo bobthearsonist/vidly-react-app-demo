dece462d0ed8425430b9cad4da963be4a3958434
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-06-14 10:46:46 -0400

    actual movie table

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,42 +1,62 @@
 import React, { Component } from "react";
-// import { getMovies } from "~/services/fakeMovieService";
+import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
-  state = { movie: null };
+  movie = {
+    _id: 123456,
+    title: "DieHard",
+    genre: "shit",
+    stock: 1,
+    rate: "$1",
+  };
+  state = { movies: getMovies() };
+
+  handleAdd = (movie) => {
+    this.setState({ movies: this.state.movies.set(movie.id, movie) });
+  };
+
+  handleDelete = (movie) => {
+    this.setState({ movies: this.state.movies.set(movie.id, movie) });
+  };
+
   render() {
     return (
-      <table class="table">
+      <table className="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Title</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Rate</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {this.state.movies.map((movie, index) => {
+            return this.buildTableEntry(movie);
+          })}
         </tbody>
       </table>
     );
   }
+
+  buildTableEntry = (movie) => {
+    const {
+      _id: id,
+      title,
+      genre,
+      numberInStock: stock,
+      dailyRentalRate: rate,
+    } = movie;
+    console.log(movie);
+    return (
+      <tr key={id}>
+        <th scope="row">{title}</th>
+        <td>{genre.name}</td>
+        <td>{stock}</td>
+        <td>{rate}</td>
+      </tr>
+    );
+  };
 }
 
 export default Movies;
