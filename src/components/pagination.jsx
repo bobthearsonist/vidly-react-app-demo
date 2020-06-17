2ca8a193e03605a9de07b49290304c49873d67e3
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-06-16 20:52:45 -0400

    adding prop type checking in pagination module

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import _ from "lodash";
-
+import PropTypes from "prop-types";
 const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
   const numberOfPages = itemsCount / pageSize + 1;
 
@@ -22,6 +22,13 @@ const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
 };
 
 export default Pagination;
