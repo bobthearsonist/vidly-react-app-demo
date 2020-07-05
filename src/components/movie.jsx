30b7f08de86d03836948bdeadcccfb1fdc5e485e
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-07-05 15:04:12 -0400

    adding save button

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Movie({ match }) {
-  return <h1>Movie Form + {match.params.id}</h1>;
+export default function Movie({ match, history }) {
+  return (
+    <div>
+      <h1>Movie Form + {match.params.id}</h1>
+      <button
+        onClick={() => history.goBack()}
+        type="button"
+        className="btn btn-primary"
+      >
+        Save
+      </button>
+    </div>
+  );
 }
