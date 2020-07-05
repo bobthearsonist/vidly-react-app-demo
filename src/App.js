d2bb1140d70aded923c121c95d38889aa6a196d9
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-07-05 13:57:23 -0400

    adding router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movies/movies";
-import NavBar from "./components/navbar";
+import VidlyNavBar from "./components/navbar";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar />
-        <Movies />
+        <VidlyNavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/movies" component={Movies} />
+            <Redirect exact path="/" to="/movies" />
+          </Switch>
+        </div>
       </React.Fragment>
     );
   }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NavBar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="/">
         Vidly
       </a>
     </nav>
