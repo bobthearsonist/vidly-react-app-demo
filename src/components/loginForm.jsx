332e149ba8e9c01201a8f625e02bc0a5aad0b619
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-07-09 12:40:45 -0400

    filling out the form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 
 export default class loginForm extends Component {
   render() {
-    return <h1>Login Form</h1>;
+    return (
+      <div>
+        <h1>Login Form</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
   }
 }
