855a6105bcc78b0346ee0cae4726a548a6dcc9e9
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-07-10 12:52:07 -0400

    form input processing

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 
 export default class loginForm extends Component {
-  username = React.createRef();
+  state = {
+    account: { username: "", password: "" },
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
     const username = this.username.current.value;
     //call the server
-    console.log("login form submitted");
+    console.log("login form submitted " + username);
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
   };
 
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login Form</h1>
@@ -19,15 +28,23 @@ export default class loginForm extends Component {
             <label htmlFor="username">Username</label>
             <input
               autoFocus
-              ref={this.username}
               id="username"
+              name="username"
               type="text"
               className="form-control"
+              onChange={(e) => this.handleChange(e)}
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+              onChange={(e) => this.handleChange(e)}
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
