0b8b816408de12729900d8f35c86301a89da7cea
Martin Perry <bobthearsonist@users.noreply.github.com>
2022-01-09 22:41:54 -0500

    basic movie application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Movies from "./components/movies";
 
 function App() {
   return (
     <main className="container">
-      <h1>Hi</h1>
+      <Movies />
     </main>
   );
 }
---
+++ src/components/movie.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Movie extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <td>movie</td>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movie;
