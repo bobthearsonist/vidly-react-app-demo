ae5fb61d454fa73e225a63f21578778f3bfb7489
Martin Perry <mperr01@gmail.com>
2022-01-14 19:27:27 -0500

    add prop types

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
 const Pagination = (props) => {
   const { itemsCount, pageSize, currentPage, onPageChange } = props;
@@ -28,6 +30,13 @@ const Pagination = (props) => {
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
 };
 
 export default Pagination;
