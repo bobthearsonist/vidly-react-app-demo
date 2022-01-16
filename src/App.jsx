ba003b741ebeccb4afa62d28a10f3aeba91af6a7
Martin Perry <mperr01@gmail.com>
2022-01-16 15:48:03 -0500

    nav bar and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Movies from "./components/movies";
+import NavBar from "./components/navBar";
+import NotFound from "./components/common/notFound";
 
 function App() {
   return (
     <main className="container-fluid">
-      <Movies />
+      <NavBar />
+      <div className="content">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={NotFound} />
+          <Route path="/rentals" component={NotFound} />
+          <Redirect from="/" to="/movies" />
+          <Redirect to="notfound" />
+          <Route path="/notfound" component={NotFound} />
+        </Switch>
+      </div>
     </main>
   );
 }
---
+++ src/components/navBar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Vidly
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="true"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/movies"
+              >
+                Movies
+              </Link>
+              <Link className="nav-link disabled" to="/customers">
+                Customers
+              </Link>
+              <Link className="nav-link disabled" to="/rentals">
+                Rentals
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
