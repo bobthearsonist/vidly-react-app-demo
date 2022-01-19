bfd2b1964ecdb75a959e48e7f1cfb8b1b1df3184
Martin Perry <mperr01@gmail.com>
2022-01-18 22:17:22 -0500

    dynamic navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 
-export default function NavBar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Vidly
-      </Link>
-      <div className="navbar" id="navbarNav">
-        <div className="navbar-nav">
-          <NavLink className="nav-link" to="/movies">
-            Movies
-          </NavLink>
-          <NavLink className="nav-link" to="/customers">
-            Customers
-          </NavLink>
-          <NavLink className="nav-link" to="/rentals">
-            Rentals
-          </NavLink>
-          <NavLink className="nav-link" to="/login">
-            Login
-          </NavLink>
+export default class NavBar extends Component {
+  state = { expand: false };
+
+  handleExpand = () => {
+    const expand = !this.state.expand;
+
+    this.setState({ expand });
+  };
+
+  render() {
+    return (
+      <nav expanded className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Vidly
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            aria-expanded="true"
+            onClick={() => this.handleExpand()}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={
+              this.state.expand ? "collapse navbar-collapse" : "navbar-collapse"
+            }
+            id="collapseNavbar"
+          >
+            <div className="navbar-nav">
+              <NavLink className="nav-link" to="/movies">
+                Movies
+              </NavLink>
+              <NavLink className="nav-link" to="/customers">
+                Customers
+              </NavLink>
+              <NavLink className="nav-link" to="/rentals">
+                Rentals
+              </NavLink>
+              <NavLink className="nav-link" to="/login">
+                Login
+              </NavLink>
+            </div>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 }
