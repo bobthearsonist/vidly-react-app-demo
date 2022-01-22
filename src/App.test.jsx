2a544e3387fb3b040cec976194ac7511ec9e24dc
Martin Perry <mperr01@gmail.com>
2022-01-22 10:23:57 -0500

    cleanup

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import NotFound from "./components/notFound";
-import { beforeAll } from "@jest/globals";
 
 beforeEach(() => {
   window.history.replaceState({}, "Test page", "/iam/a/bad/page");
@@ -16,18 +15,9 @@ beforeEach(() => {
 });
 
 test("a bad route renders not found component", async () => {
-  await waitFor(() => screen.getByRole("heading"));
-  // waitFor(() => {
   expect(NotFound);
-  // });
 });
 
 test("a bad route is replaced with notfound", async () => {
   expect(window.location.pathname).toEqual(/NotFound/i);
 });
-
-// test("renders learn react link", () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
