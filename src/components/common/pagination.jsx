7a9d964779dcb0eb700d0b34a3013283a4eac1bd
Martin Perry <mperr01@gmail.com>
2022-01-23 14:58:50 -0500

    small fixes to pagination, will need more

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import _ from "lodash";
+import { Link } from "react-router-dom";
 
-const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
+const Pagination = ({ itemsCount, pageSize, onPageChange }) => {
   const numberOfPages = itemsCount / pageSize + 1;
 
   if (Math.floor(numberOfPages) === 0) return null;
@@ -10,15 +11,14 @@ const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
   return (
     <nav aria-label="Page navigation example">
       <ul className="pagination">
-        {_.range(1, numberOfPages).map((page) => {
-          return (
-            <li key={page} className="page-item">
-              <a className="page-link" onClick={() => onPageChange(page)}>
-                {page}
-              </a>
-            </li>
-          );
-        })}
+        {_.range(1, numberOfPages).map((page) => (
+          <li key={page} className="page-item">
+            {/* TODO fix with a... NavLink? And add an active highlight*/}
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
